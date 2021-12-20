2fd07648fa7fa6d18bac01e613e6860d4b924251
Vladimir1Molchanov <92711094+Vladimir1Molchanov@users.noreply.github.com>
2021-12-20 16:36:39 +0300

    Add files via upload

---
+++ src/main/java/Cat.java
@@ -0,0 +1,13 @@
+public class Cat {
+
+    private String animal;
+
+    public String getAnimal() {
+        return animal;
+    }
+
+    public void setAnimal(String animal) {
+        this.animal = animal;
+    }
+}
+
